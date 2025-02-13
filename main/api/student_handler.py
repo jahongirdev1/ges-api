dde2f21e7d3df66c885378fcc72127fe640936bf
jahongirdev1 <jahonggir617@gmail.com>
2025-02-13 15:15:08 +0500

    change user lists

--- main/api/student_handler.py
+++ main/api/student_handler.py
@@ -5,184 +5,46 @@ from core.db import db
 
 class StudentHandler(BaseHandler):
     async def get(self):
-        products = await db.students.find({'status': 0}).to_list(None)
+        students = await db.students.find({'status': 0}).to_list(None)
 
-        for product in products:
-            product['_id'] = str(product['_id'])
+        for student in students:
+            student['_id'] = str(student['_id'])
 
-        return self.success({'items': products})
+        return self.success({'items': students})
 
     async def post(self):
-        body = self.body()
-        last_name = body.get('last_name')
-        first_name = body.get('first_name')
-        middle_name = body.get('middle_name')
-        avatar = body.get('avatar')
-        group_added_time = body.get('group_added_time')
-        invite_discount = body.get('invite_discount')
-        bonus_discount = body.get('bonus_discount')
-        school = body.get('school')
-        klass = body.get('klass')
-        new_group_id = body.get('new_group_id')
-        shift = body.get('shift')
-        group_type = body.get('group_type')
-        date = body.get('date')
-        sex = body.get('sex')
-        level_id = body.get('level_id')
-        filial_id = body.get('filial_id')
-        subject_id = body.get('subject_id')
-        address = body.get('address')
-        subject = body.get('subject')
-        phone = body.get('phone')
-        dop_phone = body.get('dop_phone')
-        phone_father = body.get('phone_father')
-        phone_mather = body.get('phone_mather')
-        email = body.get('email')
-        iin = body.get('iin')
-        copies = body.get('copies')
-        student_id_for_combination = body.get('student_id_for_combination')
-        coins = body.get('coins')
-        balance = body.get('balance')
-
         insert = await db.students.insert_one({
-            'last_name': last_name,
-            'first_name': first_name,
-            'middle_name': middle_name,
-            'avatar': avatar,
-            'group_added_time': group_added_time,
-            'invite_discount': invite_discount,
-            'bonus_discount': bonus_discount,
-            'school': school,
-            'klass': klass,
-            'new_group_id': new_group_id,
-            'shift': shift,
-            'group_type': group_type,
-            'date': date,
-            'sex': sex,
-            'level_id': level_id,
-            'filial_id': filial_id,
-            'subject_id': subject_id,
-            'address': address,
-            'subject': subject,
-            'phone': phone,
-            'dop_phone': dop_phone,
-            'phone_father': phone_father,
-            'phone_mather': phone_mather,
-            'email': email,
-            'iin': iin,
-            'copies': copies,
-            'student_id_for_combination': student_id_for_combination,
-            'coins': coins,
-            'balance': balance,
-            'next_payment': '02 Feb, 2025',
-            'next_payment_amount': 30000,
-            'coins_history': [
-                {
-                    'date': '2025-02-13',
-                    'count': 10
-                },
-                {
-                    'date': '2025-02-12',
-                    'count': 11
-                },
-                {
-                    'date': '2025-02-11',
-                    'count': 19
-                },
-                {
-                    'date': '2025-02-10',
-                    'count': 6
-                },
-                {
-                    'date': '2025-02-09',
-                    'count': 40
-                },
-                {
-                    'date': '2025-02-08',
-                    'count': 45
-                },
-                {
-                    'date': '2025-02-07',
-                    'count': 32
-                }
-            ],
-            'payment_history': [
-                {
-                    'title': 'Intermediate',
-                    'date': '2025-02-07',
-                    'amount': 14000,
-                    'isOnline': True,
-                },
-                {
-                    'title': 'Intermediate',
-                    'date': '2025-01-07',
-                    'amount': 14000,
-                    'isOnline': False,
-                }, {
-                    'title': 'Intermediate',
-                    'date': '2024-12-07',
-                    'amount': 14000,
-                    'isOnline': True,
-                }, {
-                    'title': 'Intermediate',
-                    'date': '2024-11-07',
-                    'amount': 14000,
-                    'isOnline': True,
-                }, {
-                    'title': 'Pre Intermediate',
-                    'date': '2025-02-07',
-                    'amount': 14000,
-                    'isOnline': False,
-                }, {
-                    'title': 'Intermediate',
-                    'date': '2025-02-07',
-                    'amount': 14000,
-                    'isOnline': True,
-                }, {
-                    'title': 'Intermediate',
-                    'date': '2025-02-07',
-                    'amount': 14000,
-                    'isOnline': True,
-                }, {
-                    'title': 'Intermediate',
-                    'date': '2025-02-07',
-                    'amount': 14000,
-                    'isOnline': True,
-                },
-
-            ],
-            'certificates': [
-                {
-                    'image': 'https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg',
-                    'title': 'Intermediate',
-                    'link': 'https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf'
-                },
-                {
-                    'image': 'https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg',
-                    'title': 'Pre Intermediate',
-                    'link': 'https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf'
-                },
-                {
-                    'image': 'https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg',
-                    'title': 'Elementary',
-                    'link': 'https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf'
-                },
-                {
-                    'image': 'https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg',
-                    'title': 'Beginner',
-                    'link': 'https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf'
-                },
-                {
-                    'image': 'https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg',
-                    'title': 'Primary 4',
-                    'link': 'https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf'
-                },
-                {
-                    'image': 'https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg',
-                    'title': 'Primary 3',
-                    'link': 'https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf'
-                },
-            ],
+            "last_name": "Rahmanshikov",
+            "first_name": "Jahongir",
+            "middle_name": "",
+            "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkS8v2vSpIzK2HCPWDdfZP3vbvQhEm5fxuwkNENSNSswbBoWScLb0h3GjVFqgZB9FEpSg&usqp=CAU",
+            "group_added_time": "2024-02-12T10:00:00Z",
+            "invite_discount": 10,
+            "bonus_discount": 5,
+            "school": "Example High School",
+            "klass": "10A",
+            "new_group_id": "12345",
+            "shift": "morning",
+            "group_type": "regular",
+            "date": "2024-02-12",
+            "sex": "male",
+            "level_id": "1",
+            "filial_id": "2",
+            "subject_id": "3",
+            "address": "123 Example Street",
+            "subject": "English",
+            "phone": "+77020718600",
+            "dop_phone": "+0987654321",
+            "phone_father": "+1122334455",
+            "phone_mather": "+2233445566",
+            "email": "jahonggir617@gmail.com",
+            "iin": "999999999999",
+            "copies": 2,
+            "student_id_for_combination": "98765",
+            "coins": 125,
+            "balance": 11450,
+            "next_payment": "02 Feb, 2025",
+            "next_payment_amount": 30000,
             'status': 0,
         })
 
@@ -191,16 +53,147 @@ class StudentHandler(BaseHandler):
 
         return self.success({'inserted_id': str(insert.inserted_id)})
 
-    async def delete(self):
-        body = self.body()
-        student_id = body.get('_id')
 
-        if not student_id:
-            return self.error("Student ID is required.")
+class CoinHistoryHandler(BaseHandler):
+    async def get(self):
+        coin_histories = await db.coin_history.find({}).to_list(None)
 
-        result = await db.students.delete_one({'_id': ObjectId(student_id)})
+        for coin_history in coin_histories:
+            coin_history['_id'] = str(coin_history['_id'])
 
-        if result.deleted_count == 0:
-            return self.error("Failed to delete student or student not found.")
+        return self.success({'items': coin_histores})
 
-        return self.success({'deleted_id': student_id})
+    async def post(self):
+        insert = await db.coin_history.insert_many([
+            {'date': '2025-02-13', 'count': 10},
+            {'date': '2025-02-14', 'count': 15},
+            {'date': '2025-02-15', 'count': 20},
+            {'date': '2025-02-16', 'count': 25},
+            {'date': '2025-02-17', 'count': 30},
+            {'date': '2025-02-18', 'count': 35},
+            {'date': '2025-02-19', 'count': 40},
+            {'date': '2025-02-20', 'count': 45},
+            {'date': '2025-02-21', 'count': 50},
+            {'date': '2025-02-22', 'count': 55},
+        ])
+
+        if not insert or not insert.inserted_id:
+            return self.error('Failed to create the coin.')
+
+        return self.success({'inserted_id': str(insert.inserted_id)})
+
+
+class PaymentHistoryHandler(BaseHandler):
+    async def get(self):
+        payment_histories = await db.payment_history.find({}).to_list(None)
+
+        for payment_history in payment_histories:
+            payment_history['_id'] = str(payment_history['_id'])
+
+        return self.success({'items': payment_histores})
+
+    async def post(self):
+        insert = await db.payment_history.insert_many([
+            {
+                "title": "Intermediate",
+                "date": "2025-02-07",
+                "amount": 14000,
+                "isOnline": true
+            },
+            {
+                "title": "Intermediate",
+                "date": "2025-01-07",
+                "amount": 14000,
+                "isOnline": false
+            },
+            {
+                "title": "Intermediate",
+                "date": "2024-12-07",
+                "amount": 14000,
+                "isOnline": true
+            },
+            {
+                "title": "Intermediate",
+                "date": "2024-11-07",
+                "amount": 14000,
+                "isOnline": true
+            },
+            {
+                "title": "Pre Intermediate",
+                "date": "2025-02-07",
+                "amount": 14000,
+                "isOnline": false
+            },
+            {
+                "title": "Intermediate",
+                "date": "2025-02-07",
+                "amount": 14000,
+                "isOnline": true
+            },
+            {
+                "title": "Intermediate",
+                "date": "2025-02-07",
+                "amount": 14000,
+                "isOnline": true
+            },
+            {
+                "title": "Intermediate",
+                "date": "2025-02-07",
+                "amount": 14000,
+                "isOnline": true
+            }
+        ])
+
+        if not insert or not insert.inserted_id:
+            return self.error('Failed to create the payment.')
+
+        return self.success({'inserted_id': str(insert.inserted_id)})
+
+
+class CertificateHandler(BaseHandler):
+    async def get(self):
+        certificates = await db.certificates.find({}).to_list(None)
+
+        for certificate in certificates:
+            certificate['_id'] = str(certificate['_id'])
+
+        return self.success({'items': certificates})
+
+    async def post(self):
+        insert = await db.certificates.insert_many([
+            {
+                "image": "https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg",
+                "title": "Intermediate",
+                "link": "https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf"
+            },
+            {
+                "image": "https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg",
+                "title": "Pre Intermediate",
+                "link": "https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf"
+            },
+            {
+                "image": "https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg",
+                "title": "Elementary",
+                "link": "https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf"
+            },
+            {
+                "image": "https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg",
+                "title": "Beginner",
+                "link": "https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf"
+            },
+            {
+                "image": "https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg",
+                "title": "Primary 4",
+                "link": "https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf"
+            },
+            {
+                "image": "https://cdn.vectorstock.com/i/1000v/16/73/certificate-template-vector-23631673.jpg",
+                "title": "Primary 3",
+                "link": "https://mohirdevbucket.s3.eu-central-1.amazonaws.com/certificates/cert-7f01ce2f-8912-4bd1-9683-1b5457269c23-prac--0uSpkcs7.pdf"
+            }
+        ])
+
+        if not insert or not insert.inserted_id:
+            return self.error('Failed to create the certificates.')
+
+        return self.success({'inserted_id': str(insert.inserted_id)})
--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.api.product_handler import  ProductHandler, ProductItemHandler
 from main.api.achievement_handler import AchievementHandler, AchievementsItemHandler
 from  main.api.available_course_handler import AvailableCourseHandler, AvailableCourseItemHandler
 from main.api.events_handler import EventHandler, EventsItemHandler
-from main.api.student_handler import StudentHandler
+from main.api.student_handler import StudentHandler, CoinHistoryHandler, PaymentHistoryHandler, CertificateHandler
 
 URLS = [
     (r"/api/mobile/stories", StoryHandler),
@@ -22,5 +22,14 @@ URLS = [
     (r"/api/mobile/events/([a-f0-9]{24})", EventsItemHandler),
 
     (r"/api/mobile/students", StudentHandler),
+
+    (r"/api/mobile/coin-history", CoinHistoryHandler),
+
+    (r"/api/mobile/payment-history", PaymentHistoryHandler),
+
+    (r"/api/mobile/certificates", CertificateHandler),
+
+
+
 
 ]
